29184e2ba5711165efac447b217db491f01f24c0
Carol <abc61958761@gmail.com>
2020-11-15 14:40:46 +0800

    modifiy knexfile.js

--- src/knexfile.js
+++ src/knexfile.js
@@ -7,6 +7,7 @@ let connection = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
   charset: 'utf8',
   timezone: 'UTC',
   use_env_variable: 'DATABASE_URL'
